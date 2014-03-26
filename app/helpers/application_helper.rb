8aec6ec1123fb3e37e94adcb432545237f9c6a55
Jake Gavin <jakegavin@gmail.com>
2014-03-26 12:52:54 -0700

    Write date helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def format_date_t(date)
+    date.strftime("%b %e, %Y")
+  end
+
+  def format_date_g(date)
+    date.strftime("%b %e")
+  end
+
+  def format_time_g(time)
+    time.strftime("%l:%M %P")
+  end
+
 end
